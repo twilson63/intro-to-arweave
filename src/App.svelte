48b48665693e040542651ce2b17325e54cd3026d
Tom Wilson <twilson63@gmail.com>
2021-10-27 20:39:55 +0000

    testing arweave

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,64 @@
 <script>
 	export let name;
 	let data = "";
+	let jwk = null;
+	let results = "";
+	let tokens;
 
 	const arweave = Arweave.init({
-		host: "1984-red-seahorse-2joseq26.ws-us18.gitpod.io",
+		host: "1984-rose-pig-yqdxpr5j.ws-us18.gitpod.io",
 		port: 443,
 		protocol: "https",
 	});
 
+	async function generateKey() {
+		jwk = await arweave.wallets.generate();
+		results = JSON.stringify(jwk);
+	}
 	async function createDoc() {
-		const jwk = await arweave.wallets.generate();
-		//const address = await arweave.wallets.jwkToAddress(key);
-		//const balance = await arweave.wallets.getBalance(address);
 		const transaction = await arweave.createTransaction(
 			{
 				data,
 			},
 			jwk
 		);
+		transaction.addTag("Content-Type", "text/html");
+		transaction.addTag("App", "demo");
 		await arweave.transactions.sign(transaction, jwk);
 		const response = await arweave.transactions.post(transaction);
 		console.log(transaction);
 		console.log(response.status);
+		results = "transaction: " + transaction.id;
+	}
+
+	async function sendTokens() {
+		const transaction = await arweave.createTransaction({
+			quantity: arweave.ar.arToWinston(tokens),
+			target: "hZOOp4_tS09w0k6fjkQYJxhYwFKm0ly8DPS7w2ZnRsk",
+		});
+		await arweave.transactions.sign(transaction, jwk);
+		const response = await arweave.transactions.post(transaction);
+		console.log(response.status);
+		results = `transaction id: ${transaction.id}`;
 	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>
-		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-		how to build Svelte apps.
-	</p>
+	<h1>Ar Weave Demo</h1>
+	<button on:click={generateKey}>Generate Key</button>
+	<hr />
 	<input type="text" bind:value={data} />
-	<button on:click={createDoc}>Create ArWeave Doc</button>
+	<br />
+	<button disabled={!jwk} on:click={createDoc}>Create ArWeave Doc</button>
+	<hr />
+	<input type="text" placeholder="Tokens..." bind:value={tokens} />
+	<button on:click={sendTokens}>Send Tokens</button>
+
+	<div>
+		Results
+		<hr />
+		{results}
+	</div>
 </main>
 
 <style>
