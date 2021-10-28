15caeccb7807ea9ead7a454c2fec2af0485574c4
Tom Wilson <twilson63@gmail.com>
2021-10-28 13:27:11 +0000

    bootstrapping workshop

---
+++ src/create.svelte
@@ -0,0 +1,33 @@
+<script>
+  let location = null;
+  let note = "I am here!";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(pos) {
+    location = { lat: pos.coords.latitude, lng: pos.coords.longitude };
+  }
+
+  function error(err) {
+    alert("Could not find your location: " + err.message);
+  }
+</script>
+
+<main>
+  <h2>Create a pin</h2>
+  <p>You are at: {JSON.stringify(location)}</p>
+  <form>
+    <div>
+      <label for="name">Name</label>
+      <input id="name" type="text" />
+      <p>Name of location</p>
+    </div>
+    <div>
+      <label for="note">Leave a Note:</label>
+      <textarea id="note" bind:value={note} />
+    </div>
+    <div>
+      <button>Check In</button>
+    </div>
+  </form>
+</main>
---
+++ src/pins.svelte
@@ -0,0 +1,12 @@
+<main>
+  <h2>My Pins</h2>
+  <section>
+    <aside>
+      <h3>Charleston Tech Center</h3>
+      <a href="#">Map</a>
+      <p>NOTE: ...</p>
+      <p>Date Pinned: 10/21/21</p>
+      <p>Person: ....</p>
+    </aside>
+  </section>
+</main>
