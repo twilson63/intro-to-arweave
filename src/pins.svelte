d0c9deb7047b64b6f36f141d8ef59cac76641a3b
Tom Wilson <twilson63@gmail.com>
2021-10-28 14:20:12 +0000

    setup single page server

--- src/pins.svelte
+++ src/pins.svelte
@@ -6,7 +6,21 @@
       <a href="#">Map</a>
       <p>NOTE: ...</p>
       <p>Date Pinned: 10/21/21</p>
-      <p>Person: ....</p>
+      
+    </aside>
+    <aside>
+      <h3>Raleigh Convention Center</h3>
+      <a href="#">Map</a>
+      <p>NOTE: ...</p>
+      <p>Date Pinned: 10/18/21</p>
+      
+    </aside>
+    <aside>
+      <h3>Disney World</h3>
+      <a href="#">Map</a>
+      <p>NOTE: ...</p>
+      <p>Date Pinned: 9/1/21</p>
+      
     </aside>
   </section>
 </main>
