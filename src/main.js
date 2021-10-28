916037fe9f9fda60892e78663b41b909f80be26f
Tom Wilson <twilson63@gmail.com>
2021-10-28 18:39:42 +0000

    add arweave snippet to setup arlocal connection

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import App from './app.svelte';
 
+/*
+window.arweave = Arweave.init({
+	host: 'ARWEAVE_HOST',
+	port: 443,
+	protocol: 'https'
+})
+*/
+
 const app = new App({
 	target: document.body
 });
